368d366626fffb39c171a69019b3cc5c78dd3530
Vlad-PC <vladpop20@gmail.com>
2022-11-09 16:45:56 +0200

    updated Customer to send a notification in RabbitMQ queue, so not need NotificationClient anymore

--- customer/src/main/java/com/amigoscode/customer/CustomerApplication.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(
+		scanBasePackages = {
+				"com.coodru.amqp",
+				"com.amigoscode.customer"
+		}
+)
 @EnableEurekaClient
 @EnableFeignClients(basePackages = "com.amigoscode.clients")
 public class CustomerApplication {
--- customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -4,6 +4,7 @@ import com.amigoscode.clients.fraud.FraudCheckResponse;
 import com.amigoscode.clients.fraud.FraudClient;
 import com.amigoscode.clients.notification.NotificationClient;
 import com.amigoscode.clients.notification.NotificationRequest;
+import com.coodru.amqp.RabbitMQMessageProducer;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,13 +12,15 @@ import org.springframework.stereotype.Service;
 public class CustomerService {
 
 	private final CustomerRepository customerRepository;
-	private final NotificationClient notificationClient;
 	private final FraudClient fraudClient;
+	private final RabbitMQMessageProducer rabbitMQMessageProducer;
 
-	public CustomerService(CustomerRepository customerRepository, NotificationClient notificationClient, FraudClient fraudClient) {
+	public CustomerService(CustomerRepository customerRepository, NotificationClient notificationClient,
+			FraudClient fraudClient, RabbitMQMessageProducer rabbitMQMessageProducer) {
 		this.customerRepository = customerRepository;
 		this.notificationClient = notificationClient;
 		this.fraudClient = fraudClient;
+		this.rabbitMQMessageProducer = rabbitMQMessageProducer;
 	}
 
 	public void registerCustomer(CustomerRegistrationRequest request) {
@@ -26,9 +29,12 @@ public class CustomerService {
 				.lastName(request.lastName())
 				.email(request.email())
 				.build();
+
 		// todo: check if email valid
 		// todo: check if email not taken
+
 		customerRepository.saveAndFlush(customer);
+
 		FraudCheckResponse fraudCheckResponse =
 				fraudClient.isFraudster(customer.getId());
 		if (fraudCheckResponse.isFraudster()) {
@@ -37,14 +43,17 @@ public class CustomerService {
 
 		// todo: send notification
 		// todo: make it async. i.e add to queue
-		notificationClient.sendNotification(
-				new NotificationRequest(
-						customer.getId(),
-						customer.getEmail(),
-						String.format("Hi %s, welcome to the world of Java microservices...",
-								customer.getFirstName())
-				)
-		);
+		NotificationRequest notificationRequest = new NotificationRequest(
+				customer.getId(),
+				customer.getEmail(),
+				String.format("Hi %s, welcome to the world of Java microservices...",
+						customer.getFirstName()));
 
+//		notificationClient.sendNotification(notificationRequest);
+		rabbitMQMessageProducer.publish(
+				notificationRequest,
+				"internal.exchange",
+				"internal.notification.routing-key"
+		);
 	}
 }
